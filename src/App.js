81666eb87e8872bbb44ec0c55be3eff7d1377263
avivashishta29 <avivashishta@gmail.com>
2022-12-08 21:28:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Common/Footer/footer";
+import Header from "./components/Common/Header";
+import LandingPageComponent from "./components/LandingPage/Intro";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <LandingPageComponent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
