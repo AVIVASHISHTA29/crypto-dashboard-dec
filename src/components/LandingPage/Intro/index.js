db50a483d906a84fe0643535b180edd541b40d13
avivashishta29 <avivashishta@gmail.com>
2022-12-09 21:41:23 +0530

    framer

--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -4,11 +4,12 @@ import "./styles.css";
 import gradient from "../../../assets/gradient.png";
 import iPhone from "../../../assets/iphone.png";
 import { motion } from "framer-motion";
+
 function LandingPageComponent() {
   return (
     <div className="landing-wrapper">
       <div className="landing-left">
-        <h1 className="heading-1">Track Crypto</h1>
+        <motion.h1 className="heading-1">Track Crypto</motion.h1>
         <h1 className="heading-2">Real Time.</h1>
         <p className="para">
           Track crypto through a public api in real time. Visit the dashboard to
