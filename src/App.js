382aabaddf150d67542782726d73ea92a0440e27
avivashishta29 <avivashishta@gmail.com>
2022-12-09 21:18:08 +0530

    landing component start

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      hi hello
+      <LandingPageComponent />
     </div>
   );
 }
--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import Button from "../../Common/Button/button";
+import "./styles.css";
 function LandingPageComponent() {
-  return <div>LandingPageComponent</div>;
+  return (
+    <div className="landing-wrapper">
+      <div className="landing-left">
+        <h1 className="heading-1">Track Crypto</h1>
+        <h1 className="heading-2">Real Time.</h1>
+        <p className="para">
+          Track crypto through a public api in real time. Visit the dashboard to
+          do so!
+        </p>
+        <div className="btn-flex">
+          <Button text="Dashboard" />
+          <Button text="Share App" outlined={true} />
+        </div>
+      </div>
+      <div className="landing-right"></div>
+    </div>
+  );
 }
 
 export default LandingPageComponent;
