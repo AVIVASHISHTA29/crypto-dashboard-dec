bc6d41bf386ec89355559c5d8dad014ac029180f
avivashishta29 <avivashishta@gmail.com>
2022-12-09 21:55:17 +0530

    framer motion

--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -9,16 +9,40 @@ function LandingPageComponent() {
   return (
     <div className="landing-wrapper">
       <div className="landing-left">
-        <motion.h1 className="heading-1">Track Crypto</motion.h1>
-        <h1 className="heading-2">Real Time.</h1>
-        <p className="para">
+        <motion.h1
+          className="heading-1"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Track Crypto
+        </motion.h1>
+        <motion.h1
+          className="heading-2"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          Real Time.
+        </motion.h1>
+        <motion.p
+          className="para"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
           Track crypto through a public api in real time. Visit the dashboard to
           do so!
-        </p>
-        <div className="btn-flex">
+        </motion.p>
+        <motion.div
+          className="btn-flex"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
           <Button text="Dashboard" />
           <Button text="Share App" outlined={true} />
-        </div>
+        </motion.div>
       </div>
       <div className="landing-right">
         <img src={gradient} className="gradient" />
