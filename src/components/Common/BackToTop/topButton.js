cb8c0a5db21cf3a91e224a157107abfc1559ccfd
avivashishta29 <avivashishta@gmail.com>
2022-12-15 21:08:36 +0530

    pagination

--- src/components/Common/BackToTop/topButton.js
+++ src/components/Common/BackToTop/topButton.js
@@ -10,8 +10,8 @@ function TopButton() {
 
   function scrollFunction() {
     if (
-      document.body.scrollTop > 100 ||
-      document.documentElement.scrollTop > 100
+      document.body.scrollTop > 300 ||
+      document.documentElement.scrollTop > 300
     ) {
       mybutton.style.display = "flex";
     } else {
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import TopButton from "../components/Common/BackToTop/topButton";
 import Header from "../components/Common/Header";
 import Loader from "../components/Common/Loader/loader";
+import PaginationComponent from "../components/Dashboard/Pagination/pagination";
 import SearchComponent from "../components/Dashboard/Search/search";
 import TabsComponent from "../components/Dashboard/Tabs/tabs";
 
@@ -10,6 +11,18 @@ function DashboardPage() {
   const [loading, setLoading] = useState(false);
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [paginatedCoins, setPaginatedCoins] = useState([]);
+  const handlePageChange = (event, value) => {
+    console.log("Page number1 ", value);
+    setPageNumber(value);
+    // Page 1 - [0,10)
+    // Page 2 - [10,20)
+    // Page 3 - [20,30)
+    var startingIndex = (value - 1) * 10;
+    setPaginatedCoins(coins.slice(startingIndex, startingIndex + 10));
+  };
+
   const onChange = (e) => {
     setSearch(e.target.value);
   };
@@ -38,6 +51,7 @@ function DashboardPage() {
         console.log("RESPONSE>>>", response);
         if (response.status == 200) {
           setCoins(response.data);
+          setPaginatedCoins(response.data.slice(0, 10));
           setLoading(false);
         }
       })
@@ -56,7 +70,13 @@ function DashboardPage() {
         <div>
           <Header />
           <SearchComponent search={search} onChange={onChange} />
-          <TabsComponent coins={filteredCoins} />
+          <TabsComponent coins={search ? filteredCoins : paginatedCoins} />
+          {!search && (
+            <PaginationComponent
+              pageNumber={pageNumber}
+              handleChange={handlePageChange}
+            />
+          )}
         </div>
       )}
     </>
