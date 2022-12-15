b14d2eb95ff6a80859a0a2f80654967bd289293b
avivashishta29 <avivashishta@gmail.com>
2022-12-15 20:36:41 +0530

    back to the top + loader component

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import TopButton from "../components/Common/BackToTop/topButton";
 import Header from "../components/Common/Header";
+import Loader from "../components/Common/Loader/loader";
 import SearchComponent from "../components/Dashboard/Search/search";
 import TabsComponent from "../components/Dashboard/Tabs/tabs";
 
 function DashboardPage() {
+  const [loading, setLoading] = useState(false);
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   const onChange = (e) => {
@@ -26,6 +29,7 @@ function DashboardPage() {
 
   const getData = () => {
     // Call the API and get the data
+    setLoading(true);
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
@@ -34,19 +38,28 @@ function DashboardPage() {
         console.log("RESPONSE>>>", response);
         if (response.status == 200) {
           setCoins(response.data);
+          setLoading(false);
         }
       })
       .catch((error) => {
         console.log("ERROR>>>", error);
+        setLoading(false);
       });
   };
 
   return (
-    <div>
-      <Header />
-      <SearchComponent search={search} onChange={onChange} />
-      <TabsComponent coins={filteredCoins} />
-    </div>
+    <>
+      <TopButton />
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header />
+          <SearchComponent search={search} onChange={onChange} />
+          <TabsComponent coins={filteredCoins} />
+        </div>
+      )}
+    </>
   );
 }
 
