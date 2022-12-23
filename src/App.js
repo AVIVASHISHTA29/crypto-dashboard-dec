a96526b06c4193522b42d5892ae7f3ed3896f21d
avivashishta29 <avivashishta@gmail.com>
2022-12-23 20:44:10 +0530

    mouse follower code complete

--- src/App.js
+++ src/App.js
@@ -7,10 +7,48 @@ import ComparePage from "./pages/compare";
 import WatchlistPage from "./pages/watchlist";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 
 function App() {
+  var cursor;
+  var cursorPointer;
+
+  useEffect(() => {
+    cursor = document.getElementById("cursor");
+    cursorPointer = document.getElementById("cursor-pointer");
+
+    document.body.addEventListener("mousemove", function (e) {
+      return (
+        (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px"),
+        (cursorPointer.style.left = e.clientX + "px"),
+        (cursorPointer.style.top = e.clientY + "px")
+      );
+    });
+
+    document.body.addEventListener("mousedown", function (e) {
+      return (
+        (cursor.style.height = "0.5rem"),
+        (cursor.style.width = "0.5rem"),
+        (cursorPointer.style.height = "3rem"),
+        (cursorPointer.style.width = "3rem")
+      );
+    });
+
+    document.body.addEventListener("mouseup", function (e) {
+      return (
+        (cursor.style.height = "0.3rem"),
+        (cursor.style.width = "0.3rem"),
+        (cursorPointer.style.height = "2rem"),
+        (cursorPointer.style.width = "2rem")
+      );
+    });
+  }, []);
+
   return (
     <div className="App">
+      <div className="cursor" id="cursor" />
+      <div className="cursor-pointer" id="cursor-pointer" />
       <ToastContainer />
       <BrowserRouter>
         <Routes>
@@ -27,7 +65,6 @@ function App() {
 
 export default App;
 
-//TODO: Footer
-//TODO: Mouse Follower
+//TODO: Footer + Mouse Follower
 //TODO: SEO + Code Optimisation + Responsive
 //TODO: Resume Vetting
